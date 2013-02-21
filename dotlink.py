c9ba9c86d4451e93e24720687b6be9883e3ed854
Jon Speicher <jon.speicher@gmail.com>
2013-02-21 01:35:49 -0500

    Allow regexes in ignore filename list, add amazing ignore idiom

--- dotlink.py
+++ dotlink.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-import os, sys
+import os, re, sys
 
 repo_directory, script_filename = os.path.split(sys.argv[0])
-ignore_filenames = [script_filename]
+ignore_filenames = [script_filename, 'README', '^\.']
+ignore_regex = "(" + ")|(".join(ignore_filenames) + ")"
 repo_filenames = os.listdir(repo_directory)
-link_filenames = [filename for filename in repo_filenames if filename not in ignore_filenames]
+link_filenames = [filename for filename in repo_filenames if not re.match(ignore_regex, filename)]
+
 print ignore_filenames
+print ignore_regex
 print repo_filenames
 print link_filenames
