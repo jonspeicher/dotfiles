f54399dadff966a06e04c15cf571e9315d12de04
Jon Speicher <jon.speicher@gmail.com>
2013-02-21 01:57:48 -0500

    Reorganize and rename repo and ignore portions and identifiers

--- dotlink.py
+++ dotlink.py
@@ -3,12 +3,15 @@
 import os, re, sys
 
 repo_directory, script_filename = os.path.split(sys.argv[0])
-ignore_filenames = [script_filename, 'README', '^\.']
-ignore_regex = "(" + ")|(".join(ignore_filenames) + ")"
 repo_filenames = os.listdir(repo_directory)
-link_filenames = [filename for filename in repo_filenames if not re.match(ignore_regex, filename)]
 
-print ignore_filenames
-print ignore_regex
+# TBD: load user ignore patterns from file, append to list, put some or all in constant
+ignore_patterns = [script_filename, 'README', '^\.']
+combined_ignore_patterns = "(" + ")|(".join(ignore_patterns) + ")"
+ignore_regex = re.compile(combined_ignore_patterns)
+
+link_filenames = [filename for filename in repo_filenames if not ignore_regex.match(filename)]
+
+print ignore_patterns
 print repo_filenames
 print link_filenames
