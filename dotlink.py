d7539a6588c140dc4317efe6709e64c124a75e00
Jon Speicher <jon.speicher@gmail.com>
2013-02-21 01:07:08 -0500

    Filter ignore files out of list of files to link

--- dotlink.py
+++ dotlink.py
@@ -3,6 +3,9 @@
 import os, sys
 
 repo_directory, script_filename = os.path.split(sys.argv[0])
-files_to_ignore = [script_filename]
-files_in_repo = os.listdir(repo_directory)
-print files_to_ignore, files_in_repo
+ignore_filenames = [script_filename]
+repo_filenames = os.listdir(repo_directory)
+link_filenames = [filename for filename in repo_filenames if filename not in ignore_filenames]
+print ignore_filenames
+print repo_filenames
+print link_filenames
