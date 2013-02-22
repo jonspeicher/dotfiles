14dad217b0cbe0dbe7c8205caeed045dce7e0032
Jon Speicher <jon.speicher@gmail.com>
2013-02-22 00:28:57 -0500

    Clean up ignore filtering by replacing gratuitous list comprehension with regex

--- dotlink.py
+++ dotlink.py
@@ -7,10 +7,11 @@ repo_filenames = os.listdir(repo_directory)
 
 # TBD: load user ignore patterns from file, append to list, put some or all in constant
 ignore_patterns = [script_filename, 'README', '^\.']
-combined_ignore_patterns = "(" + ")|(".join(ignore_patterns) + ")"
-ignore_regex = re.compile(combined_ignore_patterns)
+joined_ignore_patterns = "(" + ")|(".join(ignore_patterns) + ")"
+ignore_regex = re.compile(joined_ignore_patterns)
+not_ignored = lambda filename: not ignore_regex.match(filename)
 
-link_filenames = [filename for filename in repo_filenames if not ignore_regex.match(filename)]
+link_filenames = filter(not_ignored, repo_filenames)
 
 print ignore_patterns
 print repo_filenames
