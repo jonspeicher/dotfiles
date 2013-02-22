726a022bc360484f506df8424334e6d3f4ba743c
Jon Speicher <jon.speicher@gmail.com>
2013-02-22 02:11:41 -0500

    Construct user ignore filename from script basename

--- dotlink.py
+++ dotlink.py
@@ -2,6 +2,8 @@
 
 import os, re, sys
 
+USER_IGNORE_FILENAME_TEMPLATE = '.%s_ignore'
+
 def stripped_lines(filename):
     try:
         with open(filename, 'r') as f:
@@ -11,10 +13,11 @@ def stripped_lines(filename):
     return [line.strip() for line in lines]
 
 repo_directory, script_filename = os.path.split(sys.argv[0])
+script_basename, script_extension = os.path.splitext(script_filename)
+user_ignore_filename = USER_IGNORE_FILENAME_TEMPLATE % script_basename
 repo_filenames = os.listdir(repo_directory)
 
 BUILTIN_IGNORE_PATTERNS = [script_filename, 'README', '^\.']
-user_ignore_filename = '.dotlink_ignore'
 user_ignore_patterns = stripped_lines(user_ignore_filename)
 ignore_patterns = BUILTIN_IGNORE_PATTERNS + user_ignore_patterns
 joined_ignore_patterns = "(" + ")|(".join(ignore_patterns) + ")"
