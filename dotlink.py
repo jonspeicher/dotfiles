10fdcd1b3fda8704bf062d115010b2bc94098167
Jon Speicher <jon.speicher@gmail.com>
2013-02-22 01:26:27 -0500

    Add a mechanism for reading user ignore patterns

--- dotlink.py
+++ dotlink.py
@@ -2,10 +2,21 @@
 
 import os, re, sys
 
+def stripped_lines(filename):
+    try:
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+    except IOError:
+        lines = []
+    return [line.strip() for line in lines]
+
 repo_directory, script_filename = os.path.split(sys.argv[0])
 repo_filenames = os.listdir(repo_directory)
 
 # TBD: load user ignore patterns from file, append to list, put some or all in constant
+user_ignore_filename = '.dotlink_ignore'
+user_ignore_patterns = stripped_lines(user_ignore_filename)
+print user_ignore_patterns
 ignore_patterns = [script_filename, 'README', '^\.']
 joined_ignore_patterns = "(" + ")|(".join(ignore_patterns) + ")"
 ignore_regex = re.compile(joined_ignore_patterns)
