a2a5b481d875d96e6abcc2ce7617d543be36c7af
Jon Speicher <jon.speicher@gmail.com>
2013-03-12 01:05:14 -0400

    dotlink.py creates a proper list of source filenames

--- dotlink.py
+++ dotlink.py
@@ -41,7 +41,7 @@ for_platform = make_filter(platform_patterns)
 
 unignored_filenames = filter(not_ignored, repo_filenames)
 platform_filenames = filter(for_platform, unignored_filenames)
-link_filenames = platform_filenames
+source_filenames = [os.path.join(repo_directory, filename) for filename in platform_filenames]
 
 print repo_filenames
-print link_filenames
+print source_filenames
